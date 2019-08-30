aaf4920f89eef97cce870268f12ed7967fc0c305
Cesar Viana <cesar.pviana@gmail.com>
2019-08-30 15:05:53 -0300

    Remove test

--- src/app.js
+++ src/app.js
@@ -257,15 +257,3 @@ class App {
 const app = new App()
 app.registerServiceWorker()
 app.showProgrammingView()
-
-const commands = [];
-
-for(let i=0; i<2; i++)
-{
-    commands.push('FORWARD');
-    commands.push('BACKWARD');
-    commands.push('LEFT');
-    commands.push('RIGHT');
-}
-
-app.blocks.setCommands(commands)
