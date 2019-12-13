e0f2460d60d5867a422b918af4c8de65d47be477
Cesar Viana <cesar.pviana@gmail.com>
2019-12-13 14:07:37 -0300

    Dragging pieces from bottom to programming area

---
+++ components/StartButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <svg
+      viewBox="0 0 23.784723 23.010496"
+      height="70"
+      width="70"
+      @click="toggleActive"
+    >
+      <g
+        transform="translate(-220.68502,-315.73285)"
+        id="layer1">
+        <path
+          :class="{ active: active }"
+          style="stroke:#241c1c;stroke-width:0.5"
+          d="m 243.38484,327.11993 a 11.26392,11.302183 0 0 1 -11.26392,11.30218 11.26392,11.302183 0 0 1 -11.26394,-11.30218 11.26392,11.302183 0 0 1 11.26394,-11.30218 11.26392,11.302183 0 0 1 11.26392,11.30218 z"
+          id="circle">
+        </path>
+        <path
+          style="stroke:#241c1c;stroke-width:1.0"
+          d="m 236.66137,334.39857 -4.65889,-2.67425 -4.79431,2.41066 1.04611,-5.35981 -3.72231,-3.9203 5.30544,-0.63833 2.4938,-4.83356 2.2328,4.96536 5.26359,0.933 -3.92549,3.70705 z"
+          id="start">
+        </path>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "StartButton",
+    data() {
+      return {
+        active: false
+      }
+    },
+    methods: {
+      toggleActive() {
+        this.active = !this.active
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  div {
+
+    :not(.active) {
+      fill: #9cce31;
+    }
+
+    .active {
+      fill: saddlebrown;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,115 @@
 <template>
   <div class="container">
+    <div class="dragging">
+      <draggable id="pieces"
+                 :key="pieces.length"
+                 ghost-class="ghost"
+                 easing="cubic-bezier(0.5, 0, 0, 1)"
+                 animation="150"
+                 group="shared"
+      >
+        <piece v-for="piece in pieces" :command="piece.command"></piece>
+      </draggable>
+    </div>
+    <div class="available">
+      <rope/>
+      <draggable style="display:flex; justify-content: space-around"
+                 :key="availablePieces.length"
+                 :sort="false"
+                 :group="{ name: 'shared', pull: 'clone', put: false }"
+      >
+        <piece v-for="piece in availablePieces" :command="piece.command">{{piece.id}}</piece>
+      </draggable>
+      <start-button/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  components: {
+  import draggable from 'vuedraggable'
+  import rope from '~/components/RoPE'
+  import startButton from '~/components/StartButton'
+  import piece from '~/components/Piece'
+
+  const commandTypes = {
+    FORWARD: 'forward',
+    BACKWARD: 'backward',
+    LEFT: 'left',
+    RIGHT: 'right'
+  };
+
+  export default {
+    components: {
+      draggable, rope, piece, startButton
+    },
+    data() {
+      return {
+        availablePieces: [
+          { id: 0, command: commandTypes.FORWARD },
+          { id: 1, command: commandTypes.BACKWARD },
+          { id: 2, command: commandTypes.LEFT },
+          { id: 3, command: commandTypes.RIGHT }
+        ],
+        pieces: []
+      }
+    },
+    mounted() {
+      // const commandTypesArray = Object.values(commandTypes);
+      // const numPieces = 3;//Math.random() * 20 + 1;
+      // for (let i = 0; i < numPieces; i++) {
+      //   this.pieces.push({
+      //     id: i,
+      //     command: commandTypesArray[i % commandTypesArray.length]
+      //   })
+      // }
+    },
+    methods: {}
   }
-}
 </script>
 
-<style>
+<style scoped lang="scss">
+
+  $spacingDefault: 20px;
+
+  .container {
+    height: 100vh;
+    display: flex;
+    flex-flow: column;
+
+    .dragging {
+      flex-grow: 1;
+      display: flex;
+      align-items: center;
+      overflow-y: hidden;
+      border: 1px solid white;
+      padding: $spacingDefault;
+    }
+
+    .available {
+      background: #f2f2f2;
+      height: 100px;
+      width: 100%;
+      display: flex;
+      flex-flow: row;
+      justify-content: space-around;
+      align-items: center;
+    }
+  }
+
+  #pieces {
+    margin: 0 auto;
+    display: flex;
+    border: 1px solid blue;
+    min-width: 100px;
+    min-height: 100px;
+
+    div.piece {
+      width: 83px;
+    }
+
+    .ghost {
+      visibility: hidden;
+    }
+  }
+
 </style>
