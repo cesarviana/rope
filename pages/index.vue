6fa90f5acb1735bd818bf1e06dc703b7931522e0
Cesar Viana <cesar.pviana@gmail.com>
2020-01-19 11:27:24 -0300

    Avoid going to programming page if not connected

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,55 @@
 <template>
   <div class="container">
-    <rope @click.native="search" :state="ropeState"/>
+    <rope @click.native="search" :state="ropeState" />
   </div>
 </template>
 
 <script>
-  import rope from '~/components/RoPE'
+import rope from "~/components/RoPE";
 
-  export default {
-    components: {
-      rope
-    },
-    data(){
-      return {
-        ropeState: 'sleeping'
+export default {
+  components: {
+    rope
+  },
+  data() {
+    return {
+      ropeState: "sleeping"
+    };
+  },
+  methods: {
+    async search() {
+      this.ropeState = "searching";
+      try {
+        await this.$rope.search();
+        this.goToProgrammingPageIfConnected();
+      } finally {
+        this.setSleepingState();
       }
     },
-    methods: {
-      async search() {
-        this.ropeState = 'searching';
-        try {
-          await this.$rope.search();
-          this.$router.push('/programming')
-        } catch (e) {
-          this.ropeState = 'sleeping';
-        }
+    goToProgrammingPageIfConnected() {
+      if (this.$rope.isConnected()) {
+        this.$router.push("/programming");
+      }
+    },
+    setSleepingState() {
+      if (!this.$rope.isConnected()) {
+        this.ropeState = "sleeping";
       }
     }
   }
+};
 </script>
 
 <style scoped lang="scss">
+$backgroundColor: #7eb3ae5d;
+$buttonSize: 100px;
 
-  $backgroundColor: #7eb3ae5d;
-  $buttonSize: 100px;
-
-  .container {
-    height: 100vh;
-    display: flex;
-    flex-flow: column;
-    align-items: center;
-    justify-content: center;
-    background: $backgroundColor;
-  }
-
+.container {
+  height: 100vh;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  background: $backgroundColor;
+}
 </style>
