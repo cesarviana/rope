a3258da5824ac17272e79c9a5839263b73f6443d
Cesar Viana <cesar.pviana@gmail.com>
2020-01-20 11:49:08 -0300

    Hand pointing

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <rope @click.native="search" :state="ropeState" />
+    <rope @click.native="search" :state="ropeState" :class="ropeState" />
+    <div id="handWrapper">
+      <img id="hand" @click="search" :style="{ opacity: showHand ? 1 : 0 }" src="~/assets/hand.svg"/>
+    </div>
   </div>
 </template>
 
@@ -13,12 +16,14 @@ export default {
   },
   data() {
     return {
-      ropeState: "sleeping"
+      ropeState: "sleeping",
+      showHand: false
     };
   },
   methods: {
     async search() {
       this.ropeState = "searching";
+      this.showHand = false
       try {
         await this.$rope.search();
         this.goToProgrammingPageIfConnected();
@@ -34,8 +39,20 @@ export default {
     setSleepingState() {
       if (!this.$rope.isConnected()) {
         this.ropeState = "sleeping";
+        this.showHand = true
+      }
+    },
+    setHandVisible(delay) {
+      if(this.ropeState == 'sleeping'){
+        setTimeout(()=>{
+          this.showHand = true
+        }, delay)
       }
     }
+  },
+  mounted() {
+    const delay = 4000
+    this.setHandVisible(delay)
   }
 };
 </script>
@@ -51,5 +68,31 @@ $buttonSize: 100px;
   align-items: center;
   justify-content: center;
   background: $backgroundColor;
+
+  .sleeping {
+    cursor: pointer;
+  }
+
+  #handWrapper {
+    height: 100px;
+    #hand {
+      opacity: 0;
+      transition: opacity 1s;
+      animation: upDown 4s infinite cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+  }
+
+  @keyframes upDown {
+    $deslocation: 10px;
+    0% {
+      margin-bottom: $deslocation;
+    }
+    50% {
+      margin-top: $deslocation;
+    }
+    100% {
+      margin-bottom: $deslocation;
+    }
+  }
 }
 </style>
